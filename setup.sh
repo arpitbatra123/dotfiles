a981e61a6d9ac324418fbc8d79f4a7ebad74b935
Arpit Batra <arpitbatra123@gmail.com>
2020-08-15 21:51:30 +0530

    remove vscode symlink

--- setup.sh
+++ setup.sh
@@ -3,5 +3,3 @@ cd ~
 ln -s ~/Documents/dotfiles/.zshrc .zshrc
 ln -s ~/Documents/dotfiles/.gitconfig .gitconfig
 ln -s ~/Documents/dotfiles/.gitignore .gitignore
-
-ln -s ~/Documents/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
