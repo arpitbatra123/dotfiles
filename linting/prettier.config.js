b339a1c9e2a9ba319285c29f3408ba53560782e7
Arpit Batra <arpitbatra123@gmail.com>
2021-09-17 19:02:02 +0530

    prettier details

--- linting/prettier.config.js
+++ linting/prettier.config.js
@@ -2,5 +2,9 @@ module.exports = {
   semi: true,
   singleQuote: true,
   printWidth: 120,
-  trailingComma: 'none'
+  trailingComma: 'none',
+  quoteProps: 'consistent',
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  bracketSameLine: true,
 };
--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ cd ~
 ln -s ~/Documents/dotfiles/.zshrc .zshrc
 ln -s ~/Documents/dotfiles/.gitconfig .gitconfig
 ln -s ~/Documents/dotfiles/.gitignore .gitignore
+ln -s ~/Documents/dotfiles/linting/prettier.config.js .prettier.config.js
